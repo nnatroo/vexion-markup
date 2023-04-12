ca2ffb6bed8146d376a2152ee40688f237c665d5
nnatroo <nugonatroshvili@gmail.com>
2023-04-12 17:56:29 +0400

    fixed body height size + deleted scripts folder

--- scripts/script.js
+++
@@ -1,5 +0,0 @@
-const cursor = document.querySelector('.light-cursor');
-
-document.addEventListener('mousemove', e => {
-  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
